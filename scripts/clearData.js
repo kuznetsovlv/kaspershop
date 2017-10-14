9b15d0f1bb904bcae4a257b73c0ce679b20fb95f
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2017-10-14 15:46:52 +0300

    Fixed fails at cleared project

--- scripts/clearData.js
+++ scripts/clearData.js
@@ -1,5 +1,11 @@
 import { getFileInfo, readFile, writeFile } from 'fiojs';
+import { mkdirSync } from 'fs';
 import { join } from 'path';
+
+const processError = ({ message, errno }) => {
+  console.error(message);
+  process.exit(errno);
+};
 
 const ENCODING = 'utf8';
 const FILE_NAME = 'data.json';
@@ -10,12 +16,17 @@ const TO_PATH = join(TO, FILE_NAME);
 
 console.log(`Starting copy ${FROM_PATH} into ${TO_PATH}.\n`);
 
-readFile(FROM_PATH, ENCODING)
-  .then(data => writeFile(TO_PATH, data, ENCODING))
+getFileInfo(TO)
+  .then(({ exists }) => {
+    if (!exists) {
+      console.log(`Creating directory ${TO}.\n`);
+      mkdirSync(TO);
+      console.log(`Directory ${TO} created successfully.\n`);
+    }
+
+    return readFile(FROM_PATH, ENCODING).then(data => writeFile(TO_PATH, data, ENCODING));
+  })  
   .then(
     () => console.log(`${FROM_PATH} was copied into ${TO_PATH} successfully.`),
-    (error) => {
-      console.error(error);
-      process.exit(1);
-    }
+    processError
   );
