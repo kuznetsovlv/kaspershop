1232ce71b37b2edeb6cf591684186563cf56a675
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2017-10-25 22:16:03 +0300

    Fixed server

--- src/server/onGet.js
+++ src/server/onGet.js
@@ -1,7 +1,7 @@
 import { parse } from 'url';
 import { join } from 'path';
 import { getFileInfo } from 'fiojs';
-import { sendError, sendFile } from './utils';
+import { sendError, sendFile, sendData } from './utils';
 
 export default (request, response) => {
   const server = request.server || {};
@@ -9,35 +9,39 @@ export default (request, response) => {
   const { root = __dirname } = server;
 
   const data = parse(url, true);
-  // const query = data.query || {};
+  const query = data.query || {};
   const { pathname } = data;
 
-  getFileInfo(join(root, pathname))
-    .then((data) => {
-      const { exists, fileType, path, name } = data;
+  if (Object.keys(query).length === 0) {
+    getFileInfo(join(root, pathname))
+      .then((data) => {
+        const { exists, fileType, path, name } = data;
 
-      if (!exists) {
-        sendError(response, 404, `${name} not found.`);
-      } else {
-        switch (fileType) {
-          case 'regular file':
-            return data;
-          case 'directory':
-            return getFileInfo(join(path, 'index.html'))
-              .then((data) => {
-                const { exists, fileType } = data;
-                if (!exists || fileType !== 'regular file') {
-                  sendError(response, 404);
-                } else {
-                  return data;
-                }
-              });
-          default: sendError(response, 403);
+        if (!exists) {
+          sendError(response, 404, `${name} not found.`);
+        } else {
+          switch (fileType) {
+            case 'regular file':
+              return data;
+            case 'directory':
+              return getFileInfo(join(path, 'index.html'))
+                .then((data) => {
+                  const { exists, fileType } = data;
+                  if (!exists || fileType !== 'regular file') {
+                    sendError(response, 404);
+                  } else {
+                    return data;
+                  }
+                });
+            default: sendError(response, 403);
+          }
         }
-      }
-    })
-  .then(
-    data => sendFile(response, data),
-    error => sendError(response, 500, error.message || error)
-  );
+      })
+    .then(
+      data => sendFile(response, data),
+      error => sendError(response, 500, error.message || error)
+    );
+  } else {
+    sendData(response, true);
+  }
 };
