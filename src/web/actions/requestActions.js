261ad54ceffebe8962947b43756e70c1d7a8a54f
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2017-11-04 22:44:19 +0300

    Refactored data actions

--- src/web/actions/requestActions.js
+++ src/web/actions/requestActions.js
@@ -5,12 +5,7 @@ export const DATA_ERROR = 'DATA_ERROR';
 export const SET_FIELDS = 'SET_FIELDS';
 export const SET_CATHEGORIES = 'SET_CATHEGORIES';
 export const SET_DEFAULTS = 'SET_DEFAULTS';
-export const SELECT_CATHEGORY = 'SELECT_CATHEGORY';
 export const ADD_GOODS = 'ADD_GOODS';
-export const SWITCH_CATHEGORY = 'SWITCH_CATHEGORY';
-export const ADD_GOOD_INTO_BAG = 'ADD_GOOD_INTO_BAG';
-export const REMOVE_GOOD_FROM_BAG = 'REMOVE_GOOD_FROM_BAG';
-export const DECREASE_GOOD_IN_BAG = 'DECREASE_GOOD_IN_BAG';
 
 const addRequest = () => ({ type: ADD_REQUEST });
 const setError = error => ({ type: DATA_ERROR, payload: error });
@@ -58,11 +53,6 @@ export const askDefaults = () => (dispatch) => {
   );
 };
 
-export const selectCathegory = cathegory => ({
-  type: SELECT_CATHEGORY,
-  payload: cathegory
-});
-
 export const askGoods = cathegory => (dispatch) => {
   dispatch(addRequest());
 
@@ -75,23 +65,3 @@ export const askGoods = cathegory => (dispatch) => {
     error => dispatch(setDefaults({}))
   );
 };
-
-export const switchCathegoryTo = (id, selected) => ({
-  type: SWITCH_CATHEGORY,
-  payload: { id, selected }
-});
-
-export const addGoodIntoBag = (id) => ({
-  type: ADD_GOOD_INTO_BAG,
-  payload: id
-});
-
-export const removeGoodFromBag = (id) => ({
-  type: REMOVE_GOOD_FROM_BAG,
-  payload: id
-});
-
-export const decreaseGoodInBag = (id) => ({
-  type: DECREASE_GOOD_IN_BAG,
-  payload: id
-});
---
+++ src/web/actions/simpleActions.js
@@ -0,0 +1,30 @@
+export const SELECT_CATHEGORY = 'SELECT_CATHEGORY';
+export const SWITCH_CATHEGORY = 'SWITCH_CATHEGORY';
+export const ADD_GOOD_INTO_BAG = 'ADD_GOOD_INTO_BAG';
+export const REMOVE_GOOD_FROM_BAG = 'REMOVE_GOOD_FROM_BAG';
+export const DECREASE_GOOD_IN_BAG = 'DECREASE_GOOD_IN_BAG';
+
+export const selectCathegory = cathegory => ({
+  type: SELECT_CATHEGORY,
+  payload: cathegory
+});
+
+export const switchCathegoryTo = (id, selected) => ({
+  type: SWITCH_CATHEGORY,
+  payload: { id, selected }
+});
+
+export const addGoodIntoBag = (id) => ({
+  type: ADD_GOOD_INTO_BAG,
+  payload: id
+});
+
+export const removeGoodFromBag = (id) => ({
+  type: REMOVE_GOOD_FROM_BAG,
+  payload: id
+});
+
+export const decreaseGoodInBag = (id) => ({
+  type: DECREASE_GOOD_IN_BAG,
+  payload: id
+});
