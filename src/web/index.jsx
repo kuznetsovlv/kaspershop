951f2ef665bffbd08604bdcdef2cf8f505cc19bd
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2018-01-05 23:22:26 +0300

    Added redux dev tools

--- src/web/index.jsx
+++ src/web/index.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from './reducer';
 import App from './App';
 
@@ -11,7 +12,7 @@ const { DEV } = process.env;
 
 const middlewares = DEV ? applyMiddleware(thunkMiddleware, createLogger()) : applyMiddleware(thunkMiddleware);
 
-const store = createStore(reducer, middlewares);
+const store = createStore(reducer, composeWithDevTools(middlewares));
 
 render (
   <Provider store={store}>
