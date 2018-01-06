947e04e4cbba1caccb12597574e2268fb4c59248
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2018-01-06 15:15:30 +0300

    Added jsdoc for has util

--- src/web/utils/has.js
+++ src/web/utils/has.js
@@ -1,1 +1,7 @@
+/**
+ * Checks if the object has required property and this property is own.
+ * @param {Object} obj - tested object.
+ * @param {string} prop - property` name
+ * @return {boolean}
+ */
 export default (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop);
