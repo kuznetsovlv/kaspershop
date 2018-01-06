102cfea43232eb1b6cd10188dd82a3bb9f93543b
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2018-01-07 01:06:20 +0300

    Removed warning about incorrect property in div

--- src/web/components/Price/Price.jsx
+++ src/web/components/Price/Price.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { formatPrice } from '../../utils';
 
-const Price = (props = {}) => <div { ...props }>{formatPrice(props.price || 0)}</div>;
+const Price = (props = {}) => <div className={props.className || ''}>{formatPrice(props.price || 0)}</div>;
 
 Price.propTypes = {
   price: PropTypes.number.isRequired,
