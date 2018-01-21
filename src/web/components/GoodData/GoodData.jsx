9275d27256cd18c05afa773c5a49d4fabd0e94b1
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2018-01-22 01:21:30 +0300

    Created GoodData's header

--- src/web/components/GoodData/GoodData.jsx
+++ src/web/components/GoodData/GoodData.jsx
@@ -2,18 +2,31 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import classnames from 'classnames';
+import { addGoodIntoBag } from '../../actions';
 import { combineBEMBlocks } from '../../utils';
-import { FileLoader } from '../../components';
-import { blockName } from './constants';
+import { FileLoader, Photo, Availability } from '../../components';
+import {
+  blockName,
+  headerElementName,
+  dataElementName,
+  photoElementName,
+  goodNameElementName,
+  goodNameItemElementName,
+  bagZoneElementName,
+  toBagButtonElementName,
+  inTheBagElementName
+} from './constants';
 
 import './styles.scss';
 
 export class GoodData extends Component {
   static propTypes = {
+    bag: PropTypes.objectOf(PropTypes.number).isRequired,
     good: PropTypes.object.isRequired,
     fields: PropTypes.arrayOf(PropTypes.string).isRequired,
     blockName: PropTypes.string,
-    className: PropTypes.string
+    className: PropTypes.string,
+    addGoodIntoBag: PropTypes.func
   };
 
   static defaultProps = {
@@ -22,31 +35,87 @@ export class GoodData extends Component {
     good: {},
     fields: []
   }
+
+  constructor (props) {
+    super(props);
+
+    this.addGoodHandler = this.addGoodHandler.bind(this);
+  }
+
+  addGoodHandler () {
+    const { addGoodIntoBag } = this.props;
+    const good = this.props.good || {};
+    const { id = '' } = good;
+
+    if (typeof addGoodIntoBag === 'function') {
+      addGoodIntoBag(id);
+    }
+  }
   
 
   render () {
     const { blockName: userBlockName, className } = this.props;
+    const bag = this.props.bag || {};
+    const good = this.props.good || {};
+    const {
+      id = '',
+      name,
+      photo,
+      amount = 0
+    } = good;
+
+    const amountInTheBag = bag[id] || 0;
+    const inTheBagText = `В корзине: ${amountInTheBag}`;
 
     const blocks = userBlockName ? [blockName, userBlockName] : [blockName];
 
     const blockClassName = classnames(combineBEMBlocks(blocks), { [className]: className });
+    const headerClassName = combineBEMBlocks(blocks, headerElementName);
+    const dataClassName = combineBEMBlocks(blocks, dataElementName);
+    const photoClassName = combineBEMBlocks(blocks, photoElementName);
+    const goodNameClassName = combineBEMBlocks(blocks, goodNameElementName);
+    const goodNameItemClassName = combineBEMBlocks(blocks, goodNameItemElementName);
+    const bagZoneClassName = combineBEMBlocks(blocks, bagZoneElementName);
+    const toBagButtonClassName = combineBEMBlocks(blocks, toBagButtonElementName);
+    const inTheBagClassName = combineBEMBlocks(blocks, inTheBagElementName);
     
     return (
       <div className={blockClassName}>
-        Data
+        <div className={headerClassName}>
+          <div className={photoElementName}>
+            <Photo src={photo} />
+          </div>
+
+          <div className={goodNameClassName}>
+            <div className={goodNameItemClassName}>{name}</div>
+            <div className={goodNameItemClassName}>
+              <Availability amount={amount} />
+            </div>
+          </div>
+
+          <div className={bagZoneClassName}>
+            <input type="button" onClick={this.addGoodHandler} className={toBagButtonClassName} value="Добавить в корзину" />
+            <div className={inTheBagClassName}>{inTheBagText}</div>
+          </div>
+        </div>
+
+        <div className={dataClassName}>Data</div>
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ data }) => {
+const mapStateToProps = ({ data = {}, bag = {} }) => {
   const fields = data.fields || [];
 
-  return { fields };
+  return {
+    bag,
+    fields
+  };
 }
 
 const mapDispatchToPops = dispatch => ({
-  
+  addGoodIntoBag: id => dispatch(addGoodIntoBag(id))
 });
 
 export default connect(mapStateToProps, mapDispatchToPops)(GoodData);
--- src/web/components/GoodData/constants.js
+++ src/web/components/GoodData/constants.js
@@ -1,1 +1,9 @@
 export const blockName = 'good-data';
+export const headerElementName = 'header';
+export const dataElementName = 'data';
+export const photoElementName = 'photo';
+export const goodNameElementName = 'good-name';
+export const goodNameItemElementName = 'good-name-item';
+export const bagZoneElementName = 'bag-zone';
+export const toBagButtonElementName = 'to-bag-btn';
+export const inTheBagElementName = 'in-the-bag';
